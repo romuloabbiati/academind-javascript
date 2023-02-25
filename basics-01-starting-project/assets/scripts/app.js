0dfdfe1de1bacd08f7941ed55ce6c4614c8071ce
romuloabbiati <rhabbiati@hotmail.com>
2023-02-25 14:53:10 +0000

    Assignment: variables, operators, data types

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,22 @@
-alert('This works!');
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+// this string with back ticks and ${} is called template literal
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+
+// there are many ways to write text in the form of a string, using single quotes ', double quotes " and backtick character `
+// the latter is useful to interpolate with variables. The variable must be embedded with the following ${}
+// if we've got a variable called currentNumber, to print this value as a string with backtick, the variable 
+// should be represented like so ${currentNumber}
+
+// there is a sintax in which we can escape certain elements like  backslash, for that we use double backslash
+// to escape a single quote, I must use a backslash before a single quote. Same for double quotes.
+
+// to tab, I need to use backslash before a "t" like so \t
+// to add a linebreak/newline -> \n
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+outputResult(currentResult, calculationDescription);
---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,19 @@
+// step 1:
+let userInput = 15;
+let result;
+
+// step 2:
+result = 18 + userInput; // result = 33
+
+
+// step 3;
+result = result - 5; // result = 28
+result = result * 4; // result = 112
+result = result / 2; // result = 56
+
+// step 4;
+// the variable stored in userInput has not changed
+
+// step 5;
+alert(result);
+alert(userInput);
