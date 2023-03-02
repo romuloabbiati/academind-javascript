f91d0ba25860792711ed2dd0d8b4a9b28b534c56
romuloabbiati <rhabbiati@hotmail.com>
2023-03-02 08:00:52 +0000

    adding new operators to calc and code refactoring

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -6,11 +6,41 @@ function getUserInput() {
     return parseInt(userInput.value);
 }
 
-function add() {
-    const userInputValue = getUserInput();
-    const calcDescription = `${currentValue} + ${userInputValue}`;
-    currentValue = currentValue + userInputValue;
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentValue, calcDescription);
 }
 
+function add() {
+    const userInputValue = getUserInput();
+    const initialResult = currentValue;
+    currentValue = currentValue + userInputValue;
+    createAndWriteOutput('+', initialResult, userInputValue);
+}
+
+function subtract() {
+    const userInputValue = getUserInput();
+    const initialResult = currentValue;
+    currentValue = currentValue - userInputValue;
+    createAndWriteOutput('-', initialResult, userInputValue);
+
+}
+
+function multiply() {
+    const userInputValue = getUserInput();
+    const initialResult = currentValue;
+    currentValue = currentValue * userInputValue;
+    createAndWriteOutput('*', initialResult, userInputValue);
+}
+
+function divide() {
+    const userInputValue = getUserInput();
+    const initialResult = currentValue;
+    currentValue = currentValue / userInputValue;
+    createAndWriteOutput('/', initialResult, userInputValue);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
