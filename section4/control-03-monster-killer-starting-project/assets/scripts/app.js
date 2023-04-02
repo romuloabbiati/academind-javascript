e58fb1cd595bda286abf64bf6b0e789ef34a3f68
romuloabbiati <rhabbiati@hotmail.com>
2023-04-02 08:04:36 +0100

    Controlling Loops with Break

--- section4/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ section4/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -23,6 +23,7 @@ if(isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
+let lastLoggedEntry;
 
 adjustHealthBars(chosenMaxLife);
 
@@ -229,9 +230,13 @@ function printLogHandler(){
 
     let i = 0;
     for(const logEntry of battleLog) {
-        console.log(`#${i}`);
-        for(const key in logEntry) {
-            console.log(`${key} => ${logEntry[key]}`)
+        if((!lastLoggedEntry && lastLoggedEntry !== 0) || lastLoggedEntry < i) {
+            console.log(`#${i}`);
+            for(const key in logEntry) {
+                console.log(`${key} => ${logEntry[key]}`)
+            }
+            lastLoggedEntry = i;
+            break;
         }
         i++
     }
