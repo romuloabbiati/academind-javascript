92e02e56f89426e2c99f25cf6837df11435d4dd3
romuloabbiati <rhabbiati@hotmail.com>
2023-04-04 08:54:59 +0100

    difference between var let const

---
+++ section5/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,16 @@
+var myName = 'Romulo';
+
+if(myName === 'Romulo') {
+   let hobbies = ['sports', 'cooking'];
+   console.log(hobbies);
+}
+
+function greet() {
+    var age = 36;
+    var myName = 'Isabela';
+    console.log(myName, age);
+}
+
+console.log(myName, hobbies);
+
+greet();
