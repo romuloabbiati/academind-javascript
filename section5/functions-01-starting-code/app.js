0e79f9604361f1e320f08e22d44121cd40bdc47a
romuloabbiati <rhabbiati@hotmail.com>
2023-04-09 18:49:27 +0100

    Anomymous functions

--- section5/functions-01-starting-code/app.js
+++ section5/functions-01-starting-code/app.js
@@ -1,15 +1,18 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
+// anonymous function
+// const start = function() {
+//     console.log('Game is starting...', age);
+// }
+
+// const person = {
+//     greet: function greet() {
+//         console.log('Hello there!');
+//     }
+// }
+// console.dir(startGame);
+// person.greet();
+
+startGameBtn.addEventListener('click', function() {
     console.log('Game is starting...');
-}
-
-const person = {
-    greet: function greet() {
-        console.log('Hello there!');
-    }
-}
-
-person.greet(); // a function that is stored within an object is called method
-
-startGameBtn.addEventListener('click', startGame); // indirect call
+});
