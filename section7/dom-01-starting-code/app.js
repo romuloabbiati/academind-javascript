815861fade50798728866c08ea6af71e2c1ba7b1
romuloabbiati <rhabbiati@hotmail.com>
2023-04-22 20:48:57 +0100

    Assignment: DOM Querying

---
+++ section7/dom-01-starting-code/app.js
@@ -0,0 +1,16 @@
+const h1 = document.getElementById('main-title');
+
+h1.textContent = 'Some new title!';
+h1.style.color = 'white';
+h1.style.backgroundColor = 'black';
+
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent + ' (changed!)';
+
+// const listItemElements = document.querySelectorAll('li');
+
+const listItemElements = document.getElementsByTagName('li');
+
+for(const listItemEl of listItemElements) {
+    console.dir(listItemEl);
+}
---
+++ section7/dom-assignment-start/assignment.js
@@ -0,0 +1,17 @@
+// task 1
+let taskOne = document.getElementById('task-1');
+taskOne.style.color = 'white';
+
+let taskOneAgain = document.querySelector('li:first-of-type');
+taskOneAgain.style.backgroundColor = 'black';
+
+
+// task 2
+let taskTwo = document.querySelector('title');
+taskTwo.textContent = 'Assignment - Solved!';
+
+document.title = 'Assignment - Solved!';
+
+// task 3
+let taskThree = document.querySelector('h1');
+taskThree.textContent = 'Assignment - Solved!';
