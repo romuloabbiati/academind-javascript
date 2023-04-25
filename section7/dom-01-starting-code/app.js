64b9c5d4553170047f5f7c15f20fe7a5cd8438fc
romuloabbiati <rhabbiati@hotmail.com>
2023-04-25 04:59:56 +0100

    Styling DOM Elements

--- section7/dom-01-starting-code/app.js
+++ section7/dom-01-starting-code/app.js
@@ -18,3 +18,23 @@
 const ul = document.body.firstElementChild.nextElementSibling;
 
 const firstLi = ul.firstElementChild;
+
+console.log(firstLi);
+
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+
+section.className = 'red-bg';
+
+button.addEventListener('click', () => {
+    // if(section.className === 'red-bg visible') {
+    //     section.className = 'red-bg invisible';
+    // } else {
+    //     section.className = 'red-bg visible';
+    // }
+
+    section.classList.toggle('invisible');
+})
+
+
+// section.style.backgroundColor = 'blue';
