8febcf0b842a9d03b145fbfcc3fdc1515e52f930
romuloabbiati <rhabbiati@hotmail.com>
2023-05-02 08:08:35 +0100

    Creating arrays

---
+++ section8/arrays-01-starting-code/app.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// const moreNumbers = new Array(5, 2); // []
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers);
+
+const list = document.querySelectorAll('li');
+console.log(list);
+
+const moreNumbers = Array.from(list);
+console.log(moreNumbers);
