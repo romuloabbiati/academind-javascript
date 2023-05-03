c2985e319d2c2b0c0068948cf178046a79b2a3f5
romuloabbiati <rhabbiati@hotmail.com>
2023-05-03 06:06:11 +0100

    Types of data that can be stored in arrays

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -12,3 +12,15 @@ console.log(list);
 
 const moreNumbers = Array.from(list);
 console.log(moreNumbers);
+
+const hobbies = ['sports', 'cooking'];
+
+const personal = [36, 'Romulo', {moreDetails: []}];
+
+const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+
+for(const data of analyticsData) {
+    for(const dataPoint of data) {
+        console.log(dataPoint);
+    }
+}
