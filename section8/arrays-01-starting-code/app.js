aba4ef5468a3c68537cd9edbbed5e6120bdd89a7
romuloabbiati <rhabbiati@hotmail.com>
2023-05-05 07:14:23 +0100

    Adding arrays to arrays with .concat()

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -44,7 +44,8 @@
 
 
 const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
-const storedResults = testResults.slice(2);
+// const storedResults = testResults.slice(2);
+const storedResults = testResults.concat([3.99, 2]);
 
 testResults.push(5.91);
 
