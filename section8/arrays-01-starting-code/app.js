4009ac521685cd503518a177f3f4fc9c60d8adf6
romuloabbiati <rhabbiati@hotmail.com>
2023-05-07 11:29:17 +0100

    Transforming Data with .map()

--- section8/arrays-01-starting-code/app.js
+++ section8/arrays-01-starting-code/app.js
@@ -73,19 +73,41 @@
 
 // console.log(romulosIndex);
 
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+
+// const taxAdjustedPrices = [];
+
+// // for(const price of prices) {
+// //     taxAdjustedPrices.push(price * (1 + tax));
+// // }
+// // console.log(taxAdjustedPrices);
+
+// prices.forEach((price, idx, prices) => {
+//     const priceObj = { index: idx, taxAdjPrice: price * (1 + tax)};
+//     taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+
+// const taxAdjustedPrices = [];
+
+// prices.forEach((price, idx, prices) => {
+//     const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//     taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
-const taxAdjustedPrices = [];
-
-// for(const price of prices) {
-//     taxAdjustedPrices.push(price * (1 + tax));
-// }
-// console.log(taxAdjustedPrices);
-
-prices.forEach((price, idx, prices) => {
-    const priceObj = { index: idx, taxAdjPrice: price * (1 + tax)};
-    taxAdjustedPrices.push(priceObj);
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
+    const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+    return priceObj;
 });
 
 console.log(taxAdjustedPrices);
