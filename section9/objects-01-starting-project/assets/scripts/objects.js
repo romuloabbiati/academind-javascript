0d937ef3d8528d8d4c5e1eadb2149587fc04a74e
romuloabbiati <rhabbiati@hotmail.com>
2023-06-06 08:18:12 +0100

    Demo App & Shorthand Property Syntax

--- section9/objects-01-starting-project/assets/scripts/objects.js
+++ section9/objects-01-starting-project/assets/scripts/objects.js
@@ -1,30 +1,78 @@
-const movieList = document.getElementById('movie-list');
-movieList.style['background-color'] = 'red';
-movieList.style.display = 'block';
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
 
-const userChosenKeyName = 'level';
+const movies = [];
 
-let person = {
-    'first name': 'Romulo',
-    age: 36,
-    hobbies: ['coding', 'cooking'],
-    [userChosenKeyName]: '...',
-    greet: function() {
-        alert('Hi there!');
-    },
-    1.5: 'hello'
+const addMovieHandler = () => {
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    if(title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+        return;
+    }
+
+    const newMovie = {
+        info: {
+            title, // or title: title,
+            [extraName]: extraValue
+        },
+        id: Math.random()
+    }
+
+    movies.push(newMovie);
+    console.log(newMovie);
 };
 
-// person.age = 35;
-delete person.age;
-// person.age = undefined;
-// person.age = null;
-// person.greet();
+addMovieBtn.addEventListener('click', addMovieHandler);
 
-person.isAdmin = true;
 
-const keyName = 'first name';
 
-console.log(person[keyName]);
-console.log(person[1.5]);
-console.log(person);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const movieList = document.getElementById('movie-list');
+// movieList.style['background-color'] = 'red';
+// movieList.style.display = 'block';
+
+// const userChosenKeyName = 'level';
+
+// let person = {
+//     'first name': 'Romulo',
+//     age: 36,
+//     hobbies: ['coding', 'cooking'],
+//     [userChosenKeyName]: '...',
+//     greet: function() {
+//         alert('Hi there!');
+//     },
+//     1.5: 'hello'
+// };
+
+// // person.age = 35;
+// delete person.age;
+// // person.age = undefined;
+// // person.age = null;
+// // person.greet();
+
+// person.isAdmin = true;
+
+// const keyName = 'first name';
+
+// console.log(person[keyName]);
+// console.log(person[1.5]);
+// console.log(person);
