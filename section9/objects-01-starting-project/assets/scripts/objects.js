d522cc6d41a7ab043912b13485e5f94d80342a8c
romuloabbiati <rhabbiati@hotmail.com>
2023-07-08 13:05:23 +0100

    The method shorthand syntax

--- section9/objects-01-starting-project/assets/scripts/objects.js
+++ section9/objects-01-starting-project/assets/scripts/objects.js
@@ -49,7 +49,7 @@ const addMovieHandler = () => {
             [extraName]: extraValue
         },
         id: Math.random().toString(),
-        getFormattedTitle: function() {
+        getFormattedTitle() {
             return this.info.title.toUpperCase();
         }
     }
